26eb4e3b8760ef35ab88cae89cfe098097fe0f52
DlolFace <40734112+DlolFace@users.noreply.github.com>
2018-09-09 11:48:20 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -57,22 +57,22 @@ async def serverdata(ctx):
     embed.add_field(name="Members", value=len(ctx.message.server.members))
     embed.set_thumbnail(url=ctx.message.server.icon_url)
     await bot.say(embed=embed)
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def join(ctx):
     channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
+    await bot.join_voice_channel(channel)
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def leave(ctx):
     server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    await voice_client.disconnect()
+    voice_bot = bot.voice_bot_in(server)
+    await voice_bot.disconnect()
 
-@client.command(pass_context=True)
+@bot.command(pass_context=True)
 async def play(ctx, url):
     server = ctx.message.server
-    voice_client = client.voice_client_in(server)
-    player = await voice_client.create_ytdl_player(url)
+    voice_bot = bot.voice_bot_in(server)
+    player = await voice_bot.create_ytdl_player(url)
     players[server.id] = player
     player.start()
     
